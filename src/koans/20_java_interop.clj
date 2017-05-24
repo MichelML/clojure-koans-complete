216b102ba073b02703dca6d240c4b141323a1185
MichelML <michmoreau.l@gmail.com>
2017-05-23 20:11:40 -0400

    complete java interop

--- src/koans/20_java_interop.clj
+++ src/koans/20_java_interop.clj
@@ -1,19 +1,22 @@
 (ns koans.20-java-interop
   (:require [koan-engine.core :refer :all]))
 
+(defn exp [x n]
+    (reduce * (repeat n x)))
+
 (meditations
   "You may have done more with Java than you know"
-  (= __ (class "warfare")) ; hint: try typing (javadoc "warfare") in the REPL
+  (= (class (String. "warfare")) (class "warfare")) ; hint: try typing (javadoc "warfare") in the REPL
 
   "The dot signifies easy and direct Java interoperation"
-  (= __ (.toUpperCase "select * from"))
+  (= (clojure.string/upper-case "select * from") (.toUpperCase "select * from"))
 
   "But instance method calls are very different from normal functions"
-  (= ["SELECT" "FROM" "WHERE"] (map ___ ["select" "from" "where"]))
+  (= ["SELECT" "FROM" "WHERE"] (map clojure.string/upper-case ["select" "from" "where"]))
 
   "Constructing might be harder than breaking"
-  (= 10 (let [latch (java.util.concurrent.CountDownLatch. __)]
+  (= 10 (let [latch (java.util.concurrent.CountDownLatch. 10)]
           (.getCount latch)))
 
   "Static methods are slashing prices!"
-  (== __ (Math/pow 2 10)))
+  (== (exp 2 10) (Math/pow 2 10)))
